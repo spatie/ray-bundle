ba15e8e89e4078b14c04a7129781bcabccde8434
Niels <niels@spatie.be>
2021-01-25 14:36:08 +0100

    add bundle configuration

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Spatie\RayBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('spatie_ray');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->booleanNode('enable')
+                    ->defaultTrue()
+                    ->info("This settings controls whether data should be sent to Ray. By default, `ray()` will only transmit data in non-production environments.")
+                ->end()
+                ->booleanNode('send_log_calls_to_ray')
+                    ->defaultTrue()
+                    ->info("When enabled, all things logged to the application log will be sent to Ray as well.")
+                ->end()
+                ->booleanNode('send_dumps_to_ray')
+                    ->defaultTrue()
+                    ->info("When enabled, all things passed to `dump` or `dd` will be sent to Ray as well.")
+                ->end()
+                ->booleanNode('enable')
+                    ->defaultTrue()
+                    ->info("This settings controls whether data should be sent to Ray. By default, `ray()` will only transmit data in non-production environments.")
+                ->end()
+                ->scalarNode('host')
+                    ->defaultValue('localhost')
+                    ->info("The host used to communicate with the Ray app. \nFor usage in Docker on Mac or Windows, you can replace host with 'host.docker.internal' \nFor usage in Homestead on Mac or Windows, you can replace host with '10.0.2.2")
+                ->end()
+                ->integerNode('port')
+                    ->defaultValue(23517)
+                    ->info("The port number used to communicate with the Ray app.")
+                ->end()
+                ->scalarNode('remote_path')
+                    ->defaultValue(null)
+                    ->info("Absolute base path for your sites or projects in Homestead, Vagrant, Docker, or another remote development server.")
+                ->end()
+                ->scalarNode('local_path')
+                    ->defaultValue(null)
+                    ->info("Absolute base path for your sites or projects on your local computer where your IDE or code editor is running on.")
+                ->end()
+
+
+            ->end();
+
+        return $treeBuilder;
+    }
+
+}
---
+++ src/DependencyInjection/SpatieRayExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Spatie\RayBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class SpatieRayExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.xml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $definition = $container->getDefinition('spatie.ray');
+        $definition->replaceArgument(0, $config['enable']);
+        $definition->replaceArgument(1, $config['send_log_calls_to_ray']);
+        $definition->replaceArgument(2, $config['send_dumps_to_ray']);
+        $definition->replaceArgument(3, $config['host']);
+        $definition->replaceArgument(4, $config['port']);
+        $definition->replaceArgument(5, $config['remote_path']);
+        $definition->replaceArgument(6, $config['local_path']);
+
+    }
+
+    public function getNamespace(): string
+    {
+        return 'https://spatie.be/schema/dic/ray';
+    }
+
+    public function getXsdValidationBasePath(): string
+    {
+        return __DIR__.'/../Resources/config/schema';
+    }
+}
