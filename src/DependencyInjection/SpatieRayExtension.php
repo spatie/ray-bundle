db4a2b8225aa6782f317ef612c51454c01e1931e
Niels <niels@spatie.be>
2021-01-25 15:40:08 +0100

    try a different approach on the bundle config

--- src/DependencyInjection/SpatieRayExtension.php
+++ src/DependencyInjection/SpatieRayExtension.php
@@ -17,14 +17,19 @@ class SpatieRayExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = $container->getDefinition('spatie_ray');
-        $definition->replaceArgument(0, $config['enable']);
-        $definition->replaceArgument(1, $config['send_log_calls_to_ray']);
-        $definition->replaceArgument(2, $config['send_dumps_to_ray']);
-        $definition->replaceArgument(3, $config['host']);
-        $definition->replaceArgument(4, $config['port']);
-        $definition->replaceArgument(5, $config['remote_path']);
-        $definition->replaceArgument(6, $config['local_path']);
+        /**
+        $container->setParameter('ray.enable', $config['enable']);
+        $container->setParameter('ray.send_log_calls_to_ray', $config['send_log_calls_to_ray']);
+        $container->setParameter('ray.send_dumps_to_ray', $config['send_dumps_to_ray']);
+        $container->setParameter('ray.host', $config['host']);
+        $container->setParameter('ray.port', $config['port']);
+        $container->setParameter('ray.remote_path', $config['remote_path']);
+        $container->setParameter('ray.local_path', $config['local_path']);
+         */
+
+        $container->setParameter('ray.settings', $config);
+
+        $container->getDefinition('spatie_ray');
     }
 
     public function getNamespace(): string
@@ -34,6 +39,6 @@ class SpatieRayExtension extends Extension
 
     public function getXsdValidationBasePath(): string
     {
-        return __DIR__.'/../Resources/config/schema';
+        return __DIR__ . '/../Resources/config/schema';
     }
 }
